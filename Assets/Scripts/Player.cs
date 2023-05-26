5b29c9de4254c23784492452e822d8218e36d124
Kaio Barbosa Gama <kaioeduardoescar@gmail.com>
2023-05-26 00:20:13 -0400

    walk animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
+
+    private bool isWalking;
     
     // Update is called once per frame
     private void Update()
@@ -32,6 +34,7 @@ public class Player : MonoBehaviour
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime;
 
+        isWalking = moveDir != Vector3.zero;
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
     }
@@ -42,5 +45,9 @@ public class Player : MonoBehaviour
        
     }
 
+    public bool IsWalking()
+    {
+        return isWalking;
+    }
     
 }
