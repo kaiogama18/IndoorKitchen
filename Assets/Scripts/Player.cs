afc27251ccc4acf679fbc3783eb86274db7ed91b
Kaio Barbosa Gama <kaioeduardoescar@gmail.com>
2023-05-26 02:22:37 -0400

    GameInput

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,44 +5,26 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
-
-    private bool isWalking;
+    [SerializeField] private GameInput gameInput;
     
+    private bool isWalking;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+
+    }
+
     // Update is called once per frame
     private void Update()
     {
-        Vector2 inputVector = new Vector2(0, 0);
-        if(Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = +1;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = +1;
-        }
-
-        inputVector = inputVector.normalized;
-
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime;
 
         isWalking = moveDir != Vector3.zero;
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
-    }
-
-    // Start is called before the first frame update
-    private void Start()
-    {
-       
     }
 
     public bool IsWalking()
--- Assets/Scripts/PlayerAnimator.cs
+++ Assets/Scripts/PlayerAnimator.cs
@@ -8,6 +8,7 @@ public class PlayerAnimator : MonoBehaviour
     private Animator animator;
 
     [SerializeField] private Player player;
+    
     private void Awake()
     {
         animator = GetComponent<Animator>();
