e8ffe4ca1cfab17a5e58d35f4832bcf23b28f516
Kaio Barbosa Gama <kaioeduardoescar@gmail.com>
2023-05-27 13:24:36 -0400

    Update: Collision Detection - move diagonally

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -26,6 +26,39 @@ public class Player : MonoBehaviour
         float playerHeight = 2f;
         bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
         
+        if(!canMove)
+        {
+            // Cannot move toward moveDir
+
+            // Attempt only X movement
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+
+            if (canMove)
+            {
+                // Can move only on the X
+                moveDir = moveDirX;
+            }
+            else 
+            { 
+                // Cannot move only on the X 
+
+                // Attempt only Z movement
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+
+                if (canMove)
+                {
+                    // can move only on the Z
+                    moveDir = moveDirZ;
+                }
+                else
+                {
+                    // Cannot move in any direction
+                }
+            }
+        }
+
         if (canMove)
         {
             transform.position += moveDir * moveDistance;
