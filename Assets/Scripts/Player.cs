961a8c2a30d96a0721263cb439378ecd119897be
Kaio Barbosa Gama <kaioeduardoescar@gmail.com>
2023-05-27 13:12:51 -0400

    Update: CapsuleCast

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -21,12 +21,14 @@ public class Player : MonoBehaviour
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
 
-        float playerSize = .7f;
-        bool canMove = !Physics.Raycast(transform.position, moveDir, playerSize);
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = .7f;
+        float playerHeight = 2f;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
         
         if (canMove)
         {
-            transform.position += moveDir * moveSpeed * Time.deltaTime;
+            transform.position += moveDir * moveDistance;
         }
 
         isWalking = moveDir != Vector3.zero;
