5e7056f3201aea324fe77ecb907609271e58c83a
Kaio Barbosa Gama <kaioeduardoescar@gmail.com>
2023-05-30 02:20:13 -0400

    ADD: Base Interact Counter
    Function to interact with the counter

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,7 +6,9 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private GameInput gameInput;
-    
+    [SerializeField] private LayerMask countersLayerMask;
+
+
     private bool isWalking;
     private Vector3 lastInteractDir;
 
@@ -39,13 +41,13 @@ public class Player : MonoBehaviour
         }
 
         float interactDistance = 2f;
-        if(Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance))
+        if(Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, countersLayerMask))
         {
-            Debug.Log(raycastHit.transform);
-        }
-        else
-        {
-            Debug.Log("-");
+            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+            {
+                //Has ClearCounter
+                clearCounter.Interact();
+            }
         }
     }
 
